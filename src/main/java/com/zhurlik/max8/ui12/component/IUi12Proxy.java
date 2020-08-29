87341226007fdccf605ce32c8d1af704096cfa7f
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-08-29 15:06:08 +0300

    refactoring to be able to check the main logic via unit tests

--- src/main/java/com/zhurlik/max8/ui12/component/IUi12Proxy.java
+++ src/main/java/com/zhurlik/max8/ui12/component/IUi12Proxy.java
@@ -1,5 +1,6 @@
 package com.zhurlik.max8.ui12.component;
 
+import com.cycling74.max.Atom;
 import com.zhurlik.max8.ui12.client.Ui12WebSocket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
- * An interface to extract the common constants and methods.
+ * An interface to extract the common constants and default methods.
+ * The interface contains a lot of the default methods, because there is no way to create MaxObject during testing.
  *
  * @author zhurlik
  */
@@ -150,16 +152,167 @@ public interface IUi12Proxy {
     }
 
     /**
-     * Internal statuses about the WebSocket connection and the network state.
+     * Executes one of the following actions: START or STOP.
+     *
+     * @param value expected signal either 0 or 1
      */
-    enum Status {
-        // websocket
-        NOT_CONNECTED_YET,
-        CONNECTED,
-        CLOSED,
-        RECONNECTED,
-        // network
-        NETWORK_UP,
-        NETWORK_DOWN;
+    default void action(final int value) {
+        switch (Command.findBy(value)) {
+            case START:
+                start();
+                break;
+            case STOP:
+                stop();
+                break;
+            default:
+        }
     }
+
+    /**
+     * Executes one of the following actions: START or STOP.
+     *
+     * @param message either url or msg
+     * @param args
+     */
+    default void action(final String message, final Atom[] args) {
+        switch (Command.findBy(message)) {
+            case SET_URL:
+                setUrl(args);
+                break;
+            case SEND_MESSAGE:
+                toUi12Device(args);
+                break;
+            default:
+        }
+
+    }
+
+    /**
+     * Checks the current connection, makes reconnection when it's needed and sends a message to Ui12 via WebSocket.
+     *
+     * @param args incoming from Max8
+     */
+    default void toUi12Device(final Atom[] args) {
+        if (getUi12WebSocket() == null) {
+            sendStatus(Status.NOT_CONNECTED_YET);
+            return;
+        }
+
+        // to check network
+        if (!isHostAvailable()) {
+            return;
+        }
+
+        if (args != null && args.length == 1) {
+            final String message = args[0].getString();
+
+            // try make reconnection when it's possible
+            if (getUi12WebSocket().isClosed() || getUi12WebSocket().isClosing()) {
+                LOG.warn(">> Reconnecting...");
+                try {
+                    getUi12WebSocket().reconnectBlocking();
+                    sendStatus(Status.RECONNECTED);
+                } catch (InterruptedException e) {
+                    LOG.error(">> Error:", e);
+                    sendStatus(Status.CLOSED);
+                    // something is a wrong
+                    setUi12WebSocket(null);
+                    return;
+                }
+            }
+            LOG.debug(">> Sending: {}", message);
+            getUi12WebSocket().send(message);
+        }
+    }
+
+    /**
+     * Gets a single string and sets it into the url field.
+     *
+     * @param args incoming from Max8
+     */
+    default void setUrl(final Atom[] args) {
+        if (args != null && args.length == 1) {
+            setUrl(args[0].getString());
+            LOG.info(">> Url: {}", getUrl());
+            ping();
+        }
+    }
+
+    /**
+     * Makes a connection with Ui12 via WebSocket and binds the handler for reading incoming messages.
+     */
+    default void start() {
+        if (!isHostAvailable()) {
+            return;
+        }
+
+        if (getUi12WebSocket() != null && getUi12WebSocket().isOpen()) {
+            LOG.warn(">> The connection has been already opened");
+            try {
+                getUi12WebSocket().closeBlocking();
+                sendStatus(Status.CLOSED);
+            } catch (InterruptedException e) {
+                LOG.error(">> Error:", e);
+                sendStatus(Status.CLOSED);
+                // something is a wrong
+                setUi12WebSocket(null);
+            }
+        }
+
+        try {
+            setUi12WebSocket(buildWebSocketClient());
+            getUi12WebSocket().connectBlocking();
+            sendStatus(Status.CONNECTED);
+        } catch (Exception e) {
+            LOG.error(">> Error:", e);
+            // something is a wrong
+            setUi12WebSocket(null);
+            sendStatus(Status.NOT_CONNECTED_YET);
+        }
+    }
+
+    /**
+     * Makes and returns the WebSocket client for working with Ui12 device.
+     *
+     * @return a real WebSocket client
+     * @throws Exception any error
+     */
+    Ui12WebSocket buildWebSocketClient() throws Exception;
+
+    /**
+     * Closes the current WebSocket connection.
+     */
+    default void stop() {
+        if (!isHostAvailable()) {
+            // something is a wrong
+            setUi12WebSocket(null);
+        }
+
+        if (getUi12WebSocket() != null) {
+            try {
+                getUi12WebSocket().closeBlocking();
+            } catch (InterruptedException e) {
+                LOG.error(">> Error:", e);
+            }
+            sendStatus(Status.CLOSED);
+            // something is a wrong
+            setUi12WebSocket(null);
+        }
+
+        sendStatus(Status.NOT_CONNECTED_YET);
+    }
+
+    /**
+     * Setter for the WebSocket client.
+     *
+     * @param ui12WebSocket an instace of the {@link Ui12WebSocket}
+     */
+    void setUi12WebSocket(Ui12WebSocket ui12WebSocket);
+
+    /**
+     * Setter for the url of the Ui12 device.
+     *
+     * @param url in format <host:port>
+     */
+    void setUrl(String url);
 }
--- src/main/java/com/zhurlik/max8/ui12/component/Ui12Proxy.java
+++ src/main/java/com/zhurlik/max8/ui12/component/Ui12Proxy.java
@@ -11,6 +11,8 @@ import java.util.Arrays;
  * Max8 component for working with Ui12 device via WebSocket connection.
  * The main idea to read/send messages and to check connection with WebSocket Server on the Ui12 device.
  *
+ * Note: most of the logic has been moved to the default methods in the {@link IUi12Proxy}.
+ *
  * @author zhurlik@gmail.com
  */
 public class Ui12Proxy extends MaxObject implements IUi12Proxy {
@@ -40,66 +42,7 @@ public class Ui12Proxy extends MaxObject implements IUi12Proxy {
     @Override
     protected void anything(final String message, final Atom[] args) {
         LOG.debug(">> Max8 signal: message = {}, args = {}", message, Atom.toDebugString(args));
-        switch (message) {
-            case "url":
-                setUrl(args);
-                break;
-            case "msg":
-                toUi12Device(args);
-                break;
-            default:
-        }
-    }
-
-    /**
-     * Checks the current connection, makes reconnection when it's needed and sends a message to Ui12 via WebSocket.
-     *
-     * @param args incoming from Max8
-     */
-    private void toUi12Device(final Atom[] args) {
-        if (ui12WebSocket == null) {
-            sendStatus(Status.NOT_CONNECTED_YET);
-            return;
-        }
-
-        // to check network
-        if (!isHostAvailable()) {
-            return;
-        }
-
-        if (args != null && args.length == 1) {
-            final String message = args[0].getString();
-
-            // try make reconnection when it's possible
-            if (ui12WebSocket.isClosed() || ui12WebSocket.isClosing()) {
-                LOG.warn(">> Reconnecting...");
-                try {
-                    ui12WebSocket.reconnectBlocking();
-                    sendStatus(Status.RECONNECTED);
-                } catch (InterruptedException e) {
-                    LOG.error(">> Error:", e);
-                    sendStatus(Status.CLOSED);
-                    // something is a wrong
-                    ui12WebSocket = null;
-                    return;
-                }
-            }
-            LOG.debug(">> Sending: {}", message);
-            ui12WebSocket.send(message);
-        }
-    }
-
-    /**
-     * Gets a single string and sets it into the url field.
-     *
-     * @param args incoming from Max8
-     */
-    private void setUrl(final Atom[] args) {
-        if (args != null && args.length == 1) {
-            url = args[0].getString();
-            LOG.info(">> Url: {}", url);
-            ping();
-        }
+        action(message, args);
     }
 
     /**
@@ -111,71 +54,7 @@ public class Ui12Proxy extends MaxObject implements IUi12Proxy {
     protected void inlet(final int value) {
         LOG.debug(">> Max8 signal: value = {}", value);
         LOG.debug(">> Inlet:{}", getInlet());
-
-        switch (value) {
-            case 1:
-                start();
-                break;
-            case 0:
-                stop();
-                break;
-            default:
-        }
-    }
-
-    /**
-     * Makes a connection with Ui12 via WebSocket and binds the handler for reading incoming messages.
-     */
-    private void start() {
-        if (!isHostAvailable()) {
-            return;
-        }
-
-        if (ui12WebSocket != null && ui12WebSocket.isOpen()) {
-            LOG.warn(">> The connection has been already opened");
-            try {
-                ui12WebSocket.closeBlocking();
-                sendStatus(Status.CLOSED);
-            } catch (InterruptedException e) {
-                LOG.error(">> Error:", e);
-                sendStatus(Status.CLOSED);
-                // something is a wrong
-                ui12WebSocket = null;
-            }
-        }
-
-        try {
-            final String endpoint = String.format("ws://%s/socket.io/1/websocket/", url);
-            LOG.info(">> Endpoint: {}", endpoint);
-            ui12WebSocket = new Ui12WebSocket(new URI(endpoint)) {
-
-                /**
-                 * A multi-line message will be split into multiple messages.
-                 *
-                 * NOTE: there are a few custom replacements of the original message.
-                 *
-                 * @param message incoming message via WebSocket
-                 */
-                @Override
-                protected void handle(final String message) {
-                    // split by '\n'
-                    Arrays.stream(message.split("\n"))
-                            // replace '^' -> ' '
-                            .map(s -> s.replaceAll("\\^", " "))
-                            // trick with last space
-                            .map(s -> s.replaceAll(" $", " \" \""))
-                            // sending to the corresponded outlet
-                            .forEach(s -> outlet(0, new String[]{s}));
-                }
-            };
-            ui12WebSocket.connectBlocking();
-            sendStatus(Status.CONNECTED);
-        } catch (Exception e) {
-            LOG.error(">> Error:", e);
-            // something is a wrong
-            ui12WebSocket = null;
-            sendStatus(Status.NOT_CONNECTED_YET);
-        }
+        action(value);
     }
 
     @Override
@@ -183,6 +62,34 @@ public class Ui12Proxy extends MaxObject implements IUi12Proxy {
         outlet(0, new String[]{
                 String.format("STATUS: %s", status.name())
         });
+    }
+
+    @Override
+    public Ui12WebSocket buildWebSocketClient() throws Exception {
+        final String endpoint = String.format("ws://%s/socket.io/1/websocket/", getUrl());
+        LOG.info(">> Endpoint: {}", endpoint);
+
+        return new Ui12WebSocket(new URI(endpoint)) {
+
+            /**
+             * A multi-line message will be split into multiple messages.
+             *
+             * NOTE: there are a few custom replacements of the original message.
+             *
+             * @param message incoming message via WebSocket
+             */
+            @Override
+            protected void handle(final String message) {
+                // split by '\n'
+                Arrays.stream(message.split("\n"))
+                        // replace '^' -> ' '
+                        .map(s -> s.replaceAll("\\^", " "))
+                        // trick with last space
+                        .map(s -> s.replaceAll(" $", " \" \""))
+                        // sending to the corresponded outlet
+                        .forEach(s -> outlet(0, new String[]{s}));
+            }
+        };
     }
 
     @Override
@@ -195,26 +102,13 @@ public class Ui12Proxy extends MaxObject implements IUi12Proxy {
         return url;
     }
 
-    /**
-     * Closes the current WebSocket connection.
-     */
-    private void stop() {
-        if (!isHostAvailable()) {
-            // something is a wrong
-            ui12WebSocket = null;
-        }
+    @Override
+    public void setUi12WebSocket(final Ui12WebSocket ui12WebSocket) {
+        this.ui12WebSocket = ui12WebSocket;
+    }
 
-        if (ui12WebSocket != null) {
-            try {
-                ui12WebSocket.closeBlocking();
-            } catch (InterruptedException e) {
-                LOG.error(">> Error:", e);
-            }
-            sendStatus(Status.CLOSED);
-            // something is a wrong
-            ui12WebSocket = null;
-        }
-
-        sendStatus(Status.NOT_CONNECTED_YET);
+    @Override
+    public void setUrl(final String url) {
+        this.url = url;
     }
 }
