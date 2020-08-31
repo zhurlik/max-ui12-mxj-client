a526b94c98cfdb3818cf982d6f71ad41ac3e0452
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-08-31 21:29:54 +0300

    added more unit tests

---
+++ src/test/java/com/zhurlik/max8/ui12/component/UrlHandlerTest.java
@@ -0,0 +1,76 @@
+package com.zhurlik.max8.ui12.component;
+
+import com.cycling74.max.Atom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author zhurlik@gmail.com
+ */
+class UrlHandlerTest {
+    private UrlHandler test;
+
+    @BeforeEach
+    void setUp() {
+        test = new UrlHandler();
+    }
+
+    @Test
+    void testNull() throws Exception {
+        test.parse(null);
+        assertNotNull(test);
+        assertFalse(test.isValidUrl());
+        assertEquals("", test.getURI().toString());
+    }
+
+    @Test
+    void testDefault() throws Exception {
+        assertFalse(test.isValidUrl());
+        assertEquals("", test.getURI().toString());
+    }
+
+    @DisplayName("Not valid cases")
+    @ParameterizedTest(name = "{index} ==> the url is ''{0}''")
+    @EmptySource
+    @ValueSource(strings = {"123", ":", "host", "host:", ":1234", "host:port"})
+    void testParseBadValues(final String url) throws Exception {
+        // Given
+        final Atom[] args = new Atom[] {Atom.newAtom(url)};
+
+        // When
+        test.parse(args);
+
+        // Then
+        assertFalse(test.isValidUrl());
+        assertEquals("", test.getURI().toString());
+    }
+
+    @DisplayName("Valid cases")
+    @ParameterizedTest(name = "{index} ==> the url is ''{0}'' -> ''{1}''")
+    @CsvSource({
+            "host:123, ws://host:123/socket.io/1/websocket/",
+            "localhost:1256, ws://localhost:1256/socket.io/1/websocket/",
+            " localhost : 2345, ws://localhost:2345/socket.io/1/websocket/"
+    })
+    void testParseValidValues(final String url, final String expected) throws Exception {
+        // Given
+        final Atom[] args = new Atom[] {Atom.newAtom(url)};
+
+        // When
+        test.parse(args);
+
+        // Then
+        assertTrue(test.isValidUrl());
+        assertEquals(expected, test.getURI().toString());
+    }
+}
