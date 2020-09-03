d30386eecd38293f76bb057797e9015aa4a0f0d7
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-09-03 22:46:12 +0300

    more tests

--- src/test/java/com/zhurlik/max8/ui12/client/Ui12WebSocketTest.java
+++ src/test/java/com/zhurlik/max8/ui12/client/Ui12WebSocketTest.java
@@ -1,12 +1,15 @@
 package com.zhurlik.max8.ui12.client;
 
+import com.cycling74.max.Atom;
 import com.zhurlik.max8.ui12.component.MessageHandler;
+import org.java_websocket.exceptions.WebsocketNotConnectedException;
 import org.java_websocket.handshake.HandshakeImpl1Server;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.net.URI;
 import java.util.function.Consumer;
@@ -14,7 +17,9 @@ import java.util.function.Consumer;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * A simple test to check {@link Ui12WebSocket} client.
@@ -27,11 +32,15 @@ class Ui12WebSocketTest {
     private Ui12WebSocket testClient;
 
     @Mock
+    private NetworkScanner network;
+
+    @Mock
     private Consumer<String[]> outlet;
 
     @BeforeEach
     void setUp() throws Exception {
         testClient = new Ui12WebSocket(new URI(String.format("ws://localhost:%d", 1)), new MessageHandler(outlet));
+        ReflectionTestUtils.setField(testClient, "network", network);
     }
 
     @Test
@@ -62,5 +71,57 @@ class Ui12WebSocketTest {
     void testOnClose() {
         testClient.onClose(1, "test", true);
         verify(outlet).accept(new String[]{"STATUS: CLOSED"});
+        verify(network).stopPing();
+    }
+
+    @Test
+    void testUpWhenNoNetwork() {
+        when(network.isHostAvailable()).thenReturn(false);
+        testClient.up();
+        verify(network, never()).ping();
+        verify(outlet, never()).accept(any());
+    }
+
+    @Test
+    void testDownWhenNoNetwork() {
+        when(network.isHostAvailable()).thenReturn(false);
+        testClient.down();
+        verify(network, never()).stopPing();
+        verify(outlet, never()).accept(any());
+    }
+
+    @Test
+    void testUpWithNetwork() {
+        when(network.isHostAvailable()).thenReturn(true);
+        testClient.up();
+        verify(network).ping();
+        verify(outlet, times(2)).accept(new String[]{"STATUS: CLOSED"});
+    }
+
+    @Test
+    void testDownWithNetwork() {
+        when(network.isHostAvailable()).thenReturn(true);
+        testClient.down();
+        verify(network).stopPing();
+        verify(outlet).accept(new String[]{"STATUS: CLOSED"});
+    }
+
+    @Test
+    void testToUi12DeviceNoNetwork() {
+        when(network.isHostAvailable()).thenReturn(false);
+        testClient.toUi12Device(null);
+    }
+
+    @Test
+    void testToUi12DeviceWithNetwork() {
+        when(network.isHostAvailable()).thenReturn(true);
+        assertThrows(WebsocketNotConnectedException.class, () -> testClient.toUi12Device(
+                new Atom[]{Atom.newAtom("test")}));
+    }
+
+    @Test
+    void testToUi12DeviceNullWithNetwork() {
+        when(network.isHostAvailable()).thenReturn(true);
+        testClient.toUi12Device(null);
     }
 }
