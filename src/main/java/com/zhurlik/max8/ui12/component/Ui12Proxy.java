53486929ca5f32cca2ddf596c503b189caa45db4
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-09-10 21:50:20 +0300

    refactoring

--- src/main/java/com/zhurlik/max8/ui12/component/Ui12Proxy.java
+++ src/main/java/com/zhurlik/max8/ui12/component/Ui12Proxy.java
@@ -11,6 +11,8 @@ import java.util.function.Consumer;
  * Max8 component for working with Ui12 device via WebSocket connection.
  * The main idea to read/send messages and to check connection with WebSocket Server on the Ui12 device.
  *
+ * Note: this class is not covered by the unit tests because there is no way to init {@link MaxObject}.
+ *
  * @author zhurlik@gmail.com
  */
 public final class Ui12Proxy extends MaxObject {
@@ -24,7 +26,14 @@ public final class Ui12Proxy extends MaxObject {
      */
     private static final String[] OUTLET_ASSIST = new String[]{"Main Output", "Network Status", "Debug Console"};
 
+    /**
+     * Contains the main logic.
+     */
     private final CommandHandler commandHandler;
+
+    /**
+     * To have the references to the outlets of this {@link MaxObject}.
+     */
     private final Outlets outlets;
 
     /**
@@ -35,6 +44,7 @@ public final class Ui12Proxy extends MaxObject {
         setInletAssist(INLET_ASSIST);
         setOutletAssist(OUTLET_ASSIST);
 
+        // to be able to use inside others classes
         final List<Consumer<String[]>> list = new ArrayList<>(OUTLET_ASSIST.length);
         list.add((strings) -> outlet(0, strings));
         list.add((strings) -> outlet(1, strings));
@@ -46,26 +56,14 @@ public final class Ui12Proxy extends MaxObject {
     }
 
     /**
-     * Reads the messages from the inlet of the Max8 component.
+     * Redirects the messages from the inlet to the {@link CommandHandler}.
      *
-     * @param message either 'url' or 'msg'
+     * @param message expected values: either 'url' or 'msg'
      * @param args    a body of the message
      */
     @Override
     protected void anything(final String message, final Atom[] args) {
         outlets.debug(">> Max8 signal: message = {}, args = {}", message, Atom.toDebugString(args));
         commandHandler.action(message, args);
-    }
-
-    /**
-     * Reads 2 signals: 1- start and 0 - stop.
-     *
-     * @param value expected 1 or 2
-     */
-    @Override
-    protected void inlet(final int value) {
-        outlets.debug(">> Max8 signal: value = {}", value);
-        outlets.debug(">> Inlet:{}", getInlet());
-        commandHandler.action(value);
     }
 }
